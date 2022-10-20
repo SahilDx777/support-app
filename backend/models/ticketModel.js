84b2db00150ce6f1ed24ef1ffee003ced23127e1
Sahil Saxena <109143626+SahilDx777@users.noreply.github.com>
2022-10-20 22:34:35 +0530

    Add files via upload

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    product: {
+      type: String,
+      required: [true, 'Please select a product'],
+      enum: [
+        'iPhone',
+        'Macbook Pro',
+        'Google Pixel',
+        'PC',
+        'iPad',
+        'Console Box',
+      ],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter a description'],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['new', 'open', 'closed'],
+      default: 'new',
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('Ticket', ticketSchema)
